37d4dbffea3019d193e47f033234a7fc58e462b9
𝐒𝐈𝐍𝐔𝐔𝐒𝐈𝐍𝐙𝐎 ⸸ <141926736+Inamul9@users.noreply.github.com>
2026-04-03 11:01:12 +0530

    Update axiosInstance.ts

--- frontend/src/rtk/api/axiosInstance.ts
+++ frontend/src/rtk/api/axiosInstance.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const system_key = "medicway_system_key_2024"; // System key for API authentication
-// const URL = "http://localhost:5000/api/v1/";
+// const URL = "https://medicwaycare-gggd.onrender.com/api/v1/";
 
 
-const URL="https://api.mediwaycare.in/api/v1/";
+const URL="https://medicwaycare-gggd.onrender.com/api/v1/";
 const axiosInstance = axios.create({
   baseURL: URL,
   headers: {
